b8326118784648e1d2525a6cb02e56da8bf34c8a
MiguelMonr <miguemonrealjr@gmail.com>
2024-04-10 16:53:52 -0600

    Ordenando todo

--- codigos/SegundaParte.R
+++ codigos/SegundaParte.R
@@ -11,10 +11,10 @@ head(pt2, 6)
 ggplot(pt2, aes(area = Emisiones,fill = Emisiones, label= Pais, subgroup= Region)) +   geom_treemap() +
   geom_treemap_subgroup_border() +
   geom_treemap_subgroup_text(
-    place = "center", row = T, alpha = 0.5, color = "black",
+    place = "center", row = T, alpha = 0.5, color = "white",
     fontface = "italic", min.size = 0
   ) +
-  geom_treemap_text(color = "white", place = "topleft", reflow =T)
+  geom_treemap_text(color = "red", place = "topleft", reflow =T)
 #El continente que mas genera es Asia, y el que menos genera es Oceania. 
 #El pais que mas genera en Asia es Qatar, al reddor de 40 
 
